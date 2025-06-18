15c9557da477f985595b576ccb400ca9115b7aa1
Claudio Hidalgo <wilfridoh721@gmail.com>
2025-06-18 00:32:14 -0500

    primer lanzamiento para este nuevo paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerReport",                            # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Claudio Hidalgo",                       # Tu nombre
+    author_email="wilfridoh721@gmail.com",          # Tu correo electrónico
+    url="https://github.com/wilfridoh/tercerReport",     # URL del proyecto
+)
